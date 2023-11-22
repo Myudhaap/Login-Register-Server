e99526bcb5725901fda8ecacb02b31dcbcb5d621
Muhammad Yudha Adi Pratama <muhyudhapratama55@gmail.com>
2023-11-22 21:39:41 +0700

    revoke database connection

--- config/Database.js
+++ config/Database.js
@@ -2,12 +2,6 @@ import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
 export default async function connect() {
-  const mongod = await MongoMemoryServer.create();
-  const getUri = mongod.getUri();
-
-  mongoose.set("strictQuery", true);
-  // const db = await mongoose.connect(getUri);
   const db = await mongoose.connect(process.env.MONGO_URI);
-  console.log("Database Connected");
   return db;
 }
