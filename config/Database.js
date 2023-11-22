2ed6d3e992b2d2551c22128eb1bb86edaef6a354
Muhammad Yudha Adi Pratama <muhyudhapratama55@gmail.com>
2023-11-22 22:25:26 +0700

    fixed database

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
 
 export default async function connect() {
   const db = await mongoose.connect(process.env.MONGO_URI);
+  console.log("Database Connected");
   return db;
 }
